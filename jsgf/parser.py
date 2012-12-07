35a2e9503ce9f7f297913e93551e09c6a3cfd342
Alexandru Tache <hadesgames@gmail.com>
2012-12-07 03:20:25 +0200

    Devel work for parser
    closes #2
    closes #5
    closes #7
    closes #8
    closes #9
    closes #12
    closes #13
    closes #14
    closes #15
    
    ref #6

--- jsgf/parser.py
+++ jsgf/parser.py
@@ -14,23 +14,53 @@ header = header.setResultsName("Header")
 rule_name = Word(alphanums + '._')
 rule_name = rule_name.setResultsName("Rule")
 
-token = QuotedString(quoteChar='"', escChar='\\') | CharNotIn(" ;=|*+<>()[]{}/")
+token = QuotedString(quoteChar='"', escChar='\\') | CharsNotIn(";=|*+<>()[]{}/")
 token = token.setResultsName("Token")
 
-rule_ref = Literal('<') + rule_name + Literal('>')
+real_number = Combine(Word(nums) + Optional(Literal('.') + Word(nums)))
+weight = Literal('/').suppress() + real_number + Literal('/').suppress()
+weight = weight.setResultsName("Weight")
+
+
+# TODO: The tag is just a hack
+tag = Literal('{').suppress() + Word(alphanums) + Literal('}').suppress()
+tag = tag.setResultsName("Tag")
+
+rule_ref = Literal('<').suppress() + rule_name + Literal('>').suppress()
+
 
 """
-    More complex structures
+    Defining an expression. God help us.
 """
+weight_op = weight
+one_or_more_op = Literal('+')
+zero_or_more_op = Literal('*')
+tag_op = tag
+and_op = White()
+or_op = Literal('|')
 
 
+operand = rule_ref | token
+
+expression = operatorPrecedence(operand,
+    [
+     (weight_op, 1, opAssoc.RIGHT),
+     (tag_op, 1, opAssoc.LEFT),
+     (zero_or_more_op, 1, opAssoc.LEFT),
+     (one_or_more_op, 1, opAssoc.LEFT),
+     (and_op, 2, opAssoc.LEFT),
+     (or_op, 2, opAssoc.LEFT)]
+    )
+
+"""
+    Defining rest of the complex structures
+"""
 grammar = Keyword("grammar") + rule_name
-grammar = grammer.setResultsName('Grammar')
+grammar = grammar.setResultsName('Grammar')
 
 
 rule_def = Optional(Keyword("public")) + rule_ref + Literal("=") + expression
 rule_def = rule_def.setResultsName("Rule Definition")
-
 
 
 entry = (grammar | header | rule_def) + Literal(";")
